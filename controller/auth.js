0beb81d3c5b48cf0315bfb0ae7dd8691a5df6646
Anjali261 <mail2anjali.singh2610@gmail.com>
2023-07-12 01:52:17 +0530

    Authentication and Autherization

--- controller/auth.js
+++ controller/auth.js
@@ -35,3 +35,24 @@ export const register = async (req, res) =>{
 }
 
 
+//LOGING IN
+export const login = async(req,res) =>{
+    try{
+        const {email,password} = req.body;
+        const user = await User.findOne({email:email});
+        if(!user) return res.status(400).json({msg:"User doesnot exist"});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({msg:"Invalid credentials."});
+
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET);
+        //delete the password so that it is not sent to the frontend
+        delete user.password;
+        res.status(200).json({token,user});
+
+
+    }catch(err){
+        res.status(500).json({error: err.message})
+    }
+}
+
